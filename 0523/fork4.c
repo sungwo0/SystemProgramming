4166ba6fc8bb1b15a5a5fb61c7389b29afed6c23
sungwo0 <ssw4826@nvaer.com>
2025-05-23 11:06:06 +0900

    0523

---
+++ 0523/fork4.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/* 부모 프로세스가 자식 프로세스를 생성하고 끝나기를 기다린다. */
+int main()
+{
+    int pid, child, status;
+    printf("[%d] 부모 프로세스 시작 \n", getpid( ));
+    pid = fork();
+    if (pid == 0) {
+        printf("[%d] 자식 프로세스 시작 \n", getpid( ));
+        exit(1);
+    }
+    child = wait(&status); // 자식 프로세스가 끝나기를 기다린다.
+    printf("[%d] 자식 프로세스 %d 종료 \n", getpid(), child);
+    printf("\t종료 코드 %d\n", status>>8);
+}
